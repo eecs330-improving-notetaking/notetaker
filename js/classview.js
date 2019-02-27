170dd1e0dfb9b57c7d5a1dcc4159d3375c2865b2
Dillon <jdillonh@github.com>
2019-02-26 18:48:00 -0600

    almost done w making new class

--- js/classview.js
+++ js/classview.js
@@ -8,8 +8,25 @@
   already. This gives us access 
   to the 'user' global var
 */
+class schoolClass //instead of this, just require main on each page! or something
+{
+    constructor(name, number, description, notes)
+    {
+	this.name = name;
+	this.number = number; 
+	this.description = description;
+	this.notes = notes || [] //empty list of notes
+    }
 
-  
+}
+
+//get the info about our user from before / login page
+var user = JSON.parse(JSON.parse(localStorage.getItem("user")));
+
+console.log(user);
+console.log(typeof(user));
+addClassesToPage(user);
+
 function newClass()
 {
     console.log('making a new class');
@@ -26,15 +43,92 @@ function openClassPage() {
 }
 
 classes = document.getElementsByClassName("class");
-
-for(i = 0; i < classes.length; ++i) {
-    console.log(i);
-    if (classes[i].id == "new-class")
-    {
-	classes[i].onclick = newClass;
-    }
-    else
-    {
-	classes[i].onclick = openClassPage;
+if(classes != undefined){
+    for(i = 0; i < classes.length; ++i) {
+	if (classes[i].id == "new-class")
+	{
+	    classes[i].onclick = newClass;
+	}
+	else
+	{
+	    classes[i].onclick = openClassPage;
+	}
     }
 }
+
+function addClassesToPage(user)
+{
+    if( user === null || user === undefined) 
+    {
+	alert('Please change your browser to use cookies!')
+    };
+    var classesElement = document.getElementById('user-classes')
+    for(let i = 0; i<user.classes.length; ++i)
+    { 
+	currClass = user.classes[i]; 
+	template = `
+    <div class="class">
+	<div class="class-title">
+	    ${currClass.name}
+	</div>
+	<div class="class-number">
+	    ${currClass.number}
+	</div>
+	<hr>
+	<div class="class-info">
+	    ${currClass.description}
+	</div>
+	<div class="class-time">
+	<hr> <!-- TODO add currClass.meetTimes or something -->
+	    Mon: 1-2 pm <br>
+	    Wed: 1-2 pm <br>
+	    Fri: 11-12pm <br>
+	</div>
+    </div>
+    `
+	classesElement.innerHTML += template;
+    }
+}
+
+// Get the modal
+var modal = document.getElementById('new-class-popup');
+
+// Get the button that opens the modal
+
+// Get the <span> element that closes the modal
+
+// When the user clicks on the button, open the modal 
+document.getElementById("new-class").onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user X, close the modal
+var span = document.getElementById("popup-close");
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+document.getElementById("submit-new-class-btn").onclick = function(){
+    className = document.getElementById("new-class-name").value;
+    classNumber =  document.getElementById("new-class-number").value;
+    classDesc = document.getElementById("new-class-desc").value;
+    console.log(className, classNumber, classDesc);
+    if(className == "" || classNumber == "" || classDesc == "") {
+	alert("please fill in all the fields!");
+    }
+    user.classes.push(new schoolClass(className, classNumber, classDesc))
+    //save our user first!
+    //localStorage.setItem('user', JSON.stringify(user));
+    location.reload();
+    //make a new class! refresh the page or add child?
+}
+window.onbeforeunload = function(){
+    localStorage.setItem('user', JSON.stringify(user));
+};
--- js/main.js
+++ js/main.js
@@ -3,10 +3,46 @@
   loaded when user loads index.html
 */
 
-console.log('loaded main.js');
-var user = {
-    username : ""
+class schoolClass
+{
+    constructor(name, number, description, notes)
+    {
+	this.name = name;
+	this.number = number; 
+	this.description = description;
+	this.notes = notes || [] //empty list of notes
+    }
+
 }
+
+console.log('loaded main.js');
+var user = localStorage.getItem("user")
+if(user == undefined || user == null)
+{
+    console.log("new user!");
+    user = {
+	username : "",
+	classes : [
+	    new schoolClass('Human Computer Interaction',
+			    'EECS 330',
+			    'This is a great class about web interfaces'),
+	    new schoolClass('Bioethics',
+			    'PHIL 269',
+			    'This class is sooooo interesting wow')
+	],
+
+    }
+}
+else
+{
+    console.log("returning user!");
+    user = JSON.parse(user);
+}
+if(!navigator.cookieEnabled)
+{ alert("please enable cookies! \n " +
+	"Settings > Privacy > Content > Cookies > Allow 3rd Party Sites to set Cookies")
+}
+localStorage.setItem("user", JSON.stringify(user))
 
 //called by the Log In button
 function logIn()
